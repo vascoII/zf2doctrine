f334b7fce0a0f237ef3f6fe1bf54c77337f1f93c
fabrice <fabrice@fabrice-PC>
2016-03-10 01:21:20 +0100

    Add AlbumdoctrineController; Entity/Album and associate views

---
+++ README.md
@@ -0,0 +1,1 @@
+# ZF 2 with Doctrine Add
--- module/Album/config/module.config.php
+++ module/Album/config/module.config.php
@@ -1,11 +1,14 @@
 <?php
+namespace Album;
+
 return array(
     'controllers' => array(
         'invokables' => array(
             'Album\Controller\Album' => 'Album\Controller\AlbumController',
+            'Album\Controller\Albumdoctrine' => 'Album\Controller\AlbumdoctrineController',
         ),
     ),
-        'router' => array(
+    'router' => array(
         'routes' => array(
             'album' => array(
                 'type'    => 'segment',
@@ -21,12 +24,43 @@ return array(
                     ),
                 ),
             ),
+            'albumdoctrine' => array(
+                'type'    => 'segment',
+                'options' => array(
+                    'route'    => '/albumdoctrine[/:action][/:id]',
+                    'constraints' => array(
+                        'action' => '[a-zA-Z][a-zA-Z0-9_-]*',
+                        'id'     => '[0-9]+',
+                    ),
+                    'defaults' => array(
+                        'controller' => 'Album\Controller\Albumdoctrine',
+                        'action'     => 'indexdoctrine',
+                    ),
+                ),
+            ),
         ),
     ),
-
+    
     'view_manager' => array(
         'template_path_stack' => array(
             'album' => __DIR__ . '/../view',
         ),
     ),
+    
+    // Doctrine config
+    'doctrine' => array(
+        'driver' => array(
+            __NAMESPACE__ . '_driver' => array(
+                'class' => 'Doctrine\ORM\Mapping\Driver\AnnotationDriver',
+                'cache' => 'array',
+                'paths' => array(__DIR__ . '/../src/' . __NAMESPACE__ . '/Entity')
+            ),
+            'orm_default' => array(
+                'drivers' => array(
+                    __NAMESPACE__ . '\Entity' => __NAMESPACE__ . '_driver'
+                )
+            )
+        )
+    )
+    
 );
